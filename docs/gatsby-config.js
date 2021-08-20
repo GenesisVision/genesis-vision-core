c181c3c0828ce6f4cfcdb36acf1ffd64b4dc7d9b
r-kamensk <r@kamenskiy.net>
2021-08-20 19:03:46 +0300

    docs updated

---
+++ docs/gatsby-config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Genesis Vision Protocol Docs`,
+    defaultTitle: `Genesis Vision Protocol Docs`,
+    siteTitleShort: `Genesis Vision Protocol Docs`,
+    siteDescription: `Genesis Vision Protocol Documentation`,
+    siteUrl: `https://genesis.vision`,
+    siteAuthor: `@rkamenskiy`,
+    siteImage: `/platform.png`,
+    siteLanguage: `en`,
+    themeColor: `#03bdaf`,
+    basePath: `/`,
+  },
+  flags: { PRESERVE_WEBPACK_CACHE: true },
+  plugins: [
+    {
+      resolve: `@rocketseat/gatsby-theme-docs`,
+      options: {
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+        repositoryUrl: `https://github.com/rocketseat/gatsby-themes`,
+        baseDir: `examples/gatsby-theme-docs`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocketseat Gatsby Themes`,
+        short_name: `RS Gatsby Themes`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `YOUR_ANALYTICS_ID`,
+    //   },
+    // },
+    `gatsby-plugin-remove-trailing-slashes`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://rocketdocs.netlify.app`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
